81a06786ae665d6858033b34fee54d7050ae90d8
Cami2005 <105643569+Cami2005@users.noreply.github.com>
2022-05-25 00:48:29 -0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,8 +1,15 @@
-export function somar(a , b){
+export function somar (a, b) {
     return a + b;
 }
 
-export function dobro(c){
-    return c * 2;
+export function dobro (n) {
+    return n * 2;
 }
 
+export function media (a, b, c) {
+    return (a  + b + c) /3;
+}
+
+export function cinema(int, meia) {
+    return 15.0 * int + 15.0 * meia;
+}
