a57ab4d57d2e80f23906b62a93bd1b462a6b0a97
Cami2005 <105643569+Cami2005@users.noreply.github.com>
2022-05-25 00:46:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,80 +1,61 @@
 import 'dotenv/config'
+import {somar, dobro, media, cinema} from './services.js'
 
-import express from "express"
-import cors from "cors"
-
-import {somar, dobro} from './service.js'
+import express from 'express'
+import cors from 'cors'
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/dobro/:numero', (req, resp) =>{
-    try {
-        const numero = Number(req.params.numero);
-        const d = {dobro}
-        resp.send({
-            dobro:d
-        })
-    } catch (err) {
+server.get ('/dobro/:numero', (req, resp) => {
+   try{
+    const numero= req.params.numero;
+    const a= dobro(numero);
+
+    resp.send({
+        dobro:a,
+    })
+
+   } catch{
+       resp.status(404).send({
+           erro: err.message
+       })
+   }
+})
+
+
+server.get('/somar', (req, resp) => {
+   try{
+    const a= Number (req.query.a);
+    const b= Number (req.query.b);
+
+    const c= somar(a , b);
+
+    resp.send ({
+        soma: c,
+    })
+   }catch(err){
         resp.status(404).send({
-            erro:err.message
+            erro: err.message
         })
     }
 })
 
-server.get ('/somar', (req, resp)  => {
-    try {
-        const a= Number (req.query.a);
-        const b= Number (req.query.b);
-        
-        const x= a + b;
-        resp.send({
-            soma: x
-        })
-    } catch (err) {
-        resp.status(404).send({
-            erro:err.message
-        })
-    }
-})
 
-server.post('/somar', (req,resp)=> {
-    try {
-        const a = req.body.a;
-        const b = req.body.b;
 
-        const c = a + b;
-        resp.send({
-            somar:c
-        })
-    }catch (err) {
-        resp.status(404).send({
-            erro:err.message
-        })
-    }
-})
-
-server.get('dia2/corprimaria/:cor', (req,resp) =>{
-    try {
-
-        function Corpri(cor){
-
-            const mensagem = true;
+server.post('/somar', (req,resp) => {
+    try{
+        const {a, b} = req.body;
     
-            if(cor != 'vermelho' && cor != 'amarelo' && cor != 'azul')
-            mensagem = false;
-            
-            
-        }
-        resp.send({
-            resposta: Corpri
-        })
+        const x= somar (a, b);
 
-
-    } catch (err) {
+    resp.send({
+        soma: x,
+    })
+    }catch(err){
         resp.status(404).send({
-            erro:err.message
+            erro: err.message
         })
     }
 })
@@ -82,7 +63,77 @@ server.get('dia2/corprimaria/:cor', (req,resp) =>{
 
 
 
+server.get ('/dia2/corprimaria/:cor', (req, resp) => {
+    try{
+            let cor= req.params.cor;
+            let primaria= (cor=='amarelo' || cor=='azul' || cor=='vermelho');
+        
+            resp.send({
+            primaria:primaria,
+        })
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/temperatura', (req, resp) => {
+    try{
+        let temp= req.query.temp;
+    
+    let febre= (temp>=37)
+    
+    resp.send({
+        febre:febre,
+    })
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
 
 
-server.listen(process.env.PORT, 
-    ()=> console.log(`API online na porta ${process.env.PORT}`));
+
+
+server.post ('/media', (req, resp) => {
+    try{
+        const {notas: {a, b, c}} = req.body;
+  
+
+    const x= media(a , b , c);
+
+    resp.send({
+        media:x,
+    })
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+server.post('/dia2/cinema', (req, resp) => {
+    try{
+        const {int, meia, dia, nac} = req.body;
+   
+
+    const x= cinema(int, meia);
+
+    resp.send({
+        total: x,
+
+    })
+    }catch(err){
+    resp.status(404).send({
+        erro: err.message
+    })
+}
+})
+
+
+
+server.listen(process.env.PORT,
+    () => console.log (`API online na porta ${process.env.PORT}`));
+
